97a71fb286c780a50fe5ed877c9d4dcee2709c28
elTigre9 <33560876+elTigre9@users.noreply.github.com>
2018-06-15 09:01:56 -0700

    Add files via upload

---
+++ ConnectionsTest/ConnectionsTest/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectionsTest.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // create a DB Set property to add connections to database
+        public DbSet<Connections> ConnectionItems { get; set; }
+        // **this property is a table in your database
+    }
+}
